02947fae8b411adfed3069b6755d1c12c86bc472
EC2 Default User <ayaka-31014313@outlook.jp>
2022-12-30 08:03:51 +0000

    道場レッスン1(6)

--- Main4.java
+++ Main4.java
@@ -6,16 +6,24 @@ class Main4 {
 
     System.out.println("名前：");
     String firstName = scanner.next();
+
     System.out.println("名字：");
     String lastName = scanner.next();
+
     System.out.println("年齢：");
     Integer age = scanner.nextInt();
+
     System.out.println("身長(m)：");
     double height = scanner.nextDouble();
+
     System.out.println("体重(kg)：");
     double weight = scanner.nextDouble();
 
-    System.out.println("名前は" + firstName + " " + lastName + "です");
+    printData(firstName, lastName, age, height, weight);
+  }
+
+  public static void printData(String firstName, String lastName, int age, double height, double weight) {
+    System.out.println("名前は" + fullName(firstName, lastName) + "です");
     System.out.println("年齢は" + age + "歳です");
     if(age >= 20){
       System.out.println("成年者です");
@@ -25,4 +33,9 @@ class Main4 {
     System.out.println("身長は" + height + "mです");
     System.out.println("体重は" + weight + "kgです");
   }
+
+  public static String fullName(String firstName, String lastName) {
+    return firstName + " " + lastName;
+  }
+
 }
