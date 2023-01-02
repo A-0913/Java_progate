b2c0fc79ac36c052224e8233bf572e1316d62da5
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-02 05:59:14 +0000

    lesson4-4

---
+++ lessson4-4/Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person();
+    // person1に対して、helloメソッドを呼び出してください
+
+    Person person2 = new Person();
+    // person2に対して、helloメソッドを呼び出してください
+
+    person1.hello();
+    person2.hello();
+
+  }
+}
