4a8714f65f25a3ab77cf186c8aa19b91da5a780d
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-02 05:33:31 +0000

    道場レッスン1-11復習

--- lesson1-11/Main.java
+++ lesson1-11/Main.java
@@ -4,42 +4,45 @@ class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("何人分の情報を入力しますか：");
+    System.out.print("何人分の情報を入力しますか：");
     int count = scanner.nextInt();
 
     int maxAge = 0;
 
     int totalAge = 0;
 
-  for (int i = 0; i < count; i++) {
-    System.out.println(i + 1 + "人目");
+    for (int i = 0; i < count; i++) {
 
-    System.out.println("名前：");
-    String firstName = scanner.next();
+      System.out.println( i+1 + "人目");
 
-    System.out.println("名字：");
-    String lastName = scanner.next();
+      System.out.print("名前：");
+      String firstName = scanner.next();
 
-    System.out.println("年齢：");
-    int age = scanner.nextInt();
+      System.out.print("名字：");
+      String lastName = scanner.next();
 
-    if (age > maxAge) {
-      maxAge = age;
+      System.out.print("年齢：");
+      int age = scanner.nextInt();
+
+      if (age > maxAge) {
+        maxAge = age;
+      }
+
+      totalAge += age;
+
+      System.out.print("身長(m)：");
+      double height = scanner.nextDouble();
+
+      System.out.print("体重(kg)：");
+      double weight = scanner.nextDouble();
+
+      Person.printData(firstName, lastName, age, height, weight);
     }
 
-    totalAge += age;
+  System.out.println("最高年齢は" + maxAge + "歳です");
 
-    System.out.println("身長(m)：");
-    double height = scanner.nextDouble();
+  System.out.println("平均年齢は" + totalAge / count + "歳です");
 
-    System.out.println("体重(kg)：");
-    double weight = scanner.nextDouble();
-
-    System.out.println("最高年齢は" + maxAge + "歳です");
-
-    System.out.println("平均年齢は" + totalAge / count + "歳です");
-
-    Person.printData(firstName, lastName, age, height, weight);
-    }
   }
+
 }
